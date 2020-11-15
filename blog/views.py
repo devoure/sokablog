4d58e93bb39843e5538d9ce96e1fc7bd89bfac58
Athman Bakari <disguisedsandwich@gmail.com>
2020-11-15 21:30:08 +0300

    Build list and detail views

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Post
 
 # Create your views here.
+#view for retrieving all the post
+def post_list(request):
+    posts=Post.my_manager.all()
+    return render(request, 'blog/post/list.html', {'posts':posts})
+
+
+#view for displacing a single post
+def post_detail(request, year, month, day, post):
+    post = get_object_or_404(Post, slug=post,
+                             status='published',
+                             publish__year=year,
+                             publish__month=month,
+                             publish__day=day)
+    return render(request,'blog/post/detail.html',{'post':post})
+
+
