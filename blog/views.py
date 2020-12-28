2a3e1bc12b1b77a226186cb1bed8ee9966d704a8
Athman Bakari <disguisedsandwich@gmail.com>
2020-12-28 17:27:17 +0300

    Added Sending emails feature

--- blog/views.py
+++ blog/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, get_object_or_404
 from .models import Post
 from django.core.paginator import Paginator,EmptyPage,\
     PageNotAnInteger
+from .forms import EmailPostForm
+from django.core.mail import send_mail
 
 # Create your views here.
 #view for retrieving all the post
@@ -33,4 +35,26 @@ def post_detail(request, year, month, day, post):
                              publish__day=day)
     return render(request,'blog/post/detail.html',{'post':post})
 
+def post_share (request, post_id):
+    #retrieve post by id
+    post = get_object_or_404(Post, id=post_id, status='p')
+    sent = False
+
+    if request.method =='POST':
+        #form was submitted
+        form = EmailPostForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            post_url=request.build_absolute_uri(post.get_absolute_url())
+            subject='{} ({}) recommends you reading "{}"'.format(cd['name'], cd
+                                                                 ['email'], post.title)
+            message='Read "{}" at {}\n\n{}\'s comments: {}'.format(post.title, post_url,
+                                                                   cd['name'], cd['comments'])
+            send_mail(subject, message, 'disguisedsandwich@gmail.com',[cd['to']])
+            sent = True
+    else:
+        form = EmailPostForm()
+        #submit an empty form when form is initially loaded
+    return render(request, 'blog/post/share.html', {'post':post,
+                                                'form':form,'sent':sent})
 
--- soka/settings.py
+++ soka/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -113,6 +114,12 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
+
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER =os.environ['EMAIL_HOST_USER']
+EMAIL_HOST_PASSWORD = os.environ['EMAIL_HOST_PASSWORD']
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
 
 
 # Static files (CSS, JavaScript, Images)
